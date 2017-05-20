9cf52338eaeb54b26c7415feec74b15ab967f5c5
Alex <sashagajdarenko@gmail.com>
2017-05-20 07:37:06 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AtTwo::Application.config.secret_key_base = '4149966a00bde0a195b30071e69d806ea20d1243e9c7f595d7b7b0c52e3b8cd41a1ee64003425d8ede3c8d35678d3240ee9dc2435a3397289076699364ace570'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AtTwo::Application.config.session_store :cookie_store, key: '_at_two_session'

d92c66d17273a6cdae4bb349a3cb55e999790647
Olesk <oo.akabas@gmail.com>
2017-06-19 16:23:56 +0100

    Full page 'home', 'help' and 'article'

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,17 +6,29 @@ describe "StaticPages" do
 			visit '/static_pages/home'
 			expect(page).to have_content('home')
     end
+    it "should have the content 'home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title('home')
+    end
   end
   describe "Help page" do
     it "should have the content 'help'" do
 			visit '/static_pages/help'
 			expect(page).to have_content('help')
     end
+    it "should have the content 'help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title('help')
+    end
   end
   describe "Страницы Статьи" do
     it "should have the content 'Статьи'" do
 			visit '/static_pages/article'
 			expect(page).to have_content('Статьи')
+    end
+    it "should have the content 'Статьи'" do
+			visit '/static_pages/article'
+			expect(page).to have_title('Статьи')
     end
   end
 end
